22bd490ce8082229c182912f4333ca5cc606cb04
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2026-01-18 01:25:11 -0300

    reorganize folder structure to acommodate authenticated only layout

---
+++ app/(authenticated)/profile/page.tsx
@@ -0,0 +1,7 @@
+export default function ProfilePage() {
+  return (
+    <div className="h-[2000px]">
+        <h1>Profile</h1>
+    </div>
+  )
+}
--- app/(authenticated)/ticket-management/page.tsx
+++ app/(authenticated)/ticket-management/page.tsx
@@ -1,6 +1,6 @@
 export default function TicketManagementPage() {
   return (
-    <div>
+    <div className="h-[2000px]">
         <h1>Ticket Management</h1>
     </div>
   )
