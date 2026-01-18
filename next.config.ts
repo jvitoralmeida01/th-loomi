e29a3aec4bb8f613c0b4048c32b042027a061da3
jvitoralmeida01 <jvitoralmeida2001@gmail.com>
2026-01-18 01:22:39 -0300

    move nextjs dev indicator

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    position: 'bottom-right', // Options: 'bottom-right', 'bottom-left', 'top-right', 'top-left'
+  },
 };
 
 export default nextConfig;
